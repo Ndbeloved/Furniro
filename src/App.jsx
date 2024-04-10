5479e86a636bfb832c4b0d4645e8c42c3e925a75
Ndbeloved <ndbeloved001@gmail.com>
2024-04-10 08:05:03 +0100

    improving page render performance

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,23 @@ import './App.css'
 import Home from './Pages/Home/Home'
 import NavBar from './Components/NavBar/NavBar'
 import Footer from './Components/Footer/Footer'
-import Shop from './Pages/Shop/Shop'
+// import Shop from './Pages/Shop/Shop'
+import { Suspense, lazy } from 'react'
 
 
 function App() {
+  const Shop = lazy(()=> import('./Pages/Shop/Shop'))
 
   return (
     <div className='app'>
       <BrowserRouter>
         <NavBar />
+        <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path='/shop' element= {<Shop />} />
         </Routes>
+        </Suspense>
         <Footer />
       </BrowserRouter>
     </div>
