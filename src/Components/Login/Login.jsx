4ea635b7b7e3e24a346297ca98e510067c4ef0a9
Ndbeloved <ndbeloved001@gmail.com>
2024-04-14 02:30:47 +0100

    connecting google auth to backend api

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,22 +3,33 @@ import { jwtDecode } from 'jwt-decode'
 import './Login.css'
 
 const Login = () => {
-  return (
-    <section className="login">
-        <div className="wrapper">
-            <h1>Sign in</h1>
-            <GoogleLogin
-                onSuccess={(credentialResponse)=>{
-                    const credentialResponseDecoded = jwtDecode(credentialResponse.credential)
-                    console.log(credentialResponseDecoded)
-                }}
-                onError={()=>{
-                    console.log("login failed");
-                }}
-            />
-        </div>
-    </section>
-  )
+    const handleSuccess = (credentials)=>{
+        const URL = 'http://localhost:3000/user'
+        const token = credentials.credential
+        fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({token})
+        })
+        .then(res => res.json())
+        .then(data => console.log('success'))
+        .catch(error => console.log('Error parsing token to backend'))
+    }
+    return (
+        <section className="login">
+            <div className="wrapper">
+                <h1>Sign in</h1>
+                <GoogleLogin
+                    onSuccess={handleSuccess}
+                    onError={()=>{
+                        console.log("login failed");
+                    }}
+                />
+            </div>
+        </section>
+    )
 }
 
 export default Login
